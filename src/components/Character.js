c44ecd56a04be0bda4936ab867c438e896a77079
unknown <margarita.salazar.mls@gmail.com>
2021-02-19 10:17:20 -0600

    setup character function

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,8 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Character(){
+    return <div> hello world</div>
+}
+export default Character
